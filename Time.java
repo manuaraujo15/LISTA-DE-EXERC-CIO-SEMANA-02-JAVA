588f8e5fac882282e16d6d2d5da47735b84a47f8
Emanuele Fernanda Ferraz de Araújo <emanuelearaujo@ufpr.br>
2025-07-16 15:03:37 -0300

    Create Time.java

---
+++ Time.java
@@ -0,0 +1,24 @@
+class Time {
+    int hora, minuto;
+
+    Time(int h, int m) {
+        hora = h;
+        minuto = m;
+    }
+
+    boolean anterior(int h, int m) {
+        return (hora < h) || (hora == h && minuto < m);
+    }
+
+    boolean posterior(int h, int m) {
+        return (hora > h) || (hora == h && minuto > m);
+    }
+
+    boolean equals(int h, int m) {
+        return hora == h && minuto == m;
+    }
+
+    public String toString() {
+        return String.format("%02d:%02d", hora, minuto);
+    }
+}
